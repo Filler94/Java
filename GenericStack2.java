5c2117661f650ac6f6f07c9dc9a25cffd50ea486
Filler94 <64613492+Filler94@users.noreply.github.com>
2020-07-27 16:21:14 -0400

    Add files via upload

---
+++ GenericStack2.java
@@ -0,0 +1,50 @@
+package week01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class GenericStack2<E> extends ArrayList<E>
+{
+
+	
+	public int getSize()
+	{
+	
+		return super.size();
+	}
+	
+	public E peek()
+	{
+
+		return super.get(getSize() -1);
+	}
+	
+	public void push(E o)
+	{
+	
+		super.add(o);
+	}
+	
+	public E pop()
+	{
+		
+		E o = super.get(getSize() -1);
+		super.remove(getSize() -1);
+		return o;
+		}
+		
+	public boolean isEmpty()
+	{
+	
+		return super.isEmpty();
+	}
+	
+	
+	@Override
+	
+	public String toString()
+	{
+		return "stack: " + super.toString();
+	}
+}
